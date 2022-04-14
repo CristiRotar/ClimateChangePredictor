c0ed2cb8354ec262d45acdc2d6f1c5dbec3b055d
marianbalan <marianbalan06@gmail.com>
2022-04-14 19:27:16 +0300

    Added sklearn linear regression

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import csv
 import os
 
+import numpy as np
+
+from ToolRegression import ToolRegression
 
 
 def loadData(fileName, inputVariabName1, inputVariabName2, inputVariabName3, outputVariabName):
@@ -21,7 +24,7 @@ def loadData(fileName, inputVariabName1, inputVariabName2, inputVariabName3, out
             else:
                 if row[monthIndex] != 'Meteorological year' and '?' not in row[monthIndex]:
                     data.append(row)
-                    if data[line_count-1][valueIndex] == '':
+                    if data[line_count - 1][valueIndex] == '':
                         if startIndex == -1:
                             startIndex = line_count - 1
                         else:
@@ -36,16 +39,32 @@ def loadData(fileName, inputVariabName1, inputVariabName2, inputVariabName3, out
                             endIndex = -1
                     line_count += 1
         for list in indexesWithNULLValue:
-            mean = (float(data[list[0]-1][valueIndex]) + float(data[list[1]+1][valueIndex])) / 2
-            for i in range(list[0], list[1]+1):
+            mean = (float(data[list[0] - 1][valueIndex]) + float(data[list[1] + 1][valueIndex])) / 2
+            for i in range(list[0], list[1] + 1):
                 data[i][valueIndex] = mean
         selectedVariable1 = dataNames.index(inputVariabName1)
         selectedVariable2 = dataNames.index(inputVariabName2)
         selectedVariable3 = dataNames.index(inputVariabName3)
-        inputs = [[data[i][selectedVariable1], data[i][selectedVariable2], data[i][selectedVariable3]] for i in range(len(data))]
+        inputs = [[data[i][selectedVariable1], data[i][selectedVariable2], data[i][selectedVariable3]] for i in
+                  range(len(data))]
         selectedOutput = dataNames.index(outputVariabName)
         outputs = [round(float(data[i][selectedOutput]), 4) for i in range(len(data))]
         return inputs, outputs
+
+
+def splitData(inputs, outputs):
+    np.random.seed(5)
+    indexes = [i for i in range(len(inputs))]
+    trainSample = np.random.choice(indexes, int(0.8 * len(inputs)), replace=False)
+    validationSample = [i for i in indexes if not i in trainSample]
+
+    trainInputs = [inputs[i] for i in trainSample]
+    trainOutputs = [outputs[i] for i in trainSample]
+
+    validationInputs = [inputs[i] for i in validationSample]
+    validationOutputs = [outputs[i] for i in validationSample]
+
+    return trainInputs, trainOutputs, validationInputs, validationOutputs
 
 
 if __name__ == '__main__':
@@ -53,3 +72,30 @@ if __name__ == '__main__':
     filePath = os.path.join(crtDir, 'data', 'FAOSTAT_data_1-10-2022.csv')
     inputs, outputs = loadData(filePath, 'Area', 'Months', 'Year', 'Value')
 
+    areaClassification = {}
+    monthClassification = {}
+    areaIndex = 0
+    monthIndex = 0
+
+    for input in inputs:
+        if input[0] in areaClassification.keys():
+            input[0] = areaClassification[input[0]]
+        else:
+            input[0] = areaIndex
+            areaIndex += 1
+        if input[1] in monthClassification.keys():
+            input[1] = monthClassification[input[1]]
+        else:
+            input[1] = monthIndex
+            monthIndex += 1
+        input[2] = int(input[2])
+
+    x1 = [inputs[i][0] for i in range(len(inputs))]
+    x2 = [inputs[i][1] for i in range(len(inputs))]
+    x3 = [inputs[i][2] for i in range(len(inputs))]
+
+    trainInputs, trainOutputs, validationInputs, validationOutputs = splitData(inputs, outputs)
+    toolReg = ToolRegression(trainInputs, trainOutputs, validationInputs, validationOutputs)
+    w0, w1, w2, w3 = toolReg.fit()
+    computedValidationOutputs = toolReg.predict()
+    err = toolReg.error()
